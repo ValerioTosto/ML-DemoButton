8a30c74ca3dc32462342d6f5e28f6ae5f16ce61f
Boros93 <andrea_sequenzia@yahoo.it>
2019-06-24 19:08:30 +0200

    Enabled VGG16

--- net/main.py
+++ net/main.py
@@ -12,14 +12,15 @@ def training(modelName, pretrainedFlag, epochsNumber):
     train_loader,valid_loader,test_loader = load_dataset()
 
     # definizione del modello
-    choose = 0
+    choose = 1
+
     # model = get_model_squeezenet(pretrainedFlag) if choose == 0 else get_model_vgg(pretrainedFlag)
-    # model = vgg16(pretrained=pretrainedFlag)
-    model = squeezenet1_0(pretrainedFlag)
+    model = vgg16(pretrained=pretrainedFlag)
+    #model = squeezenet1_0(pretrainedFlag)
     num_class = 4
-    # model.classifier[6] = nn.Linear(4096, num_class)
-    model.classifier[1] = nn.Conv2d(512, num_class, kernel_size=(1, 1), stride=(1, 1))
-    model.num_classes = num_class
+    model.classifier[6] = nn.Linear(4096, num_class)
+    #model.classifier[1] = nn.Conv2d(512, num_class, kernel_size=(1, 1), stride=(1, 1))
+    #model.num_classes = num_class
     # fase di training 
     net = "Squeezenet model" if choose == 0 else "VGG16"
     model = trainval_classifier(model, train_loader, valid_loader, lr=0.001, exp_name=net, momentum=0.99, epochs=epochsNumber)
